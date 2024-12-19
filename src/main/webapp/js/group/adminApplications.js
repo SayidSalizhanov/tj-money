305ac929048d8485706d4554b1b7817d46f9ad3a
Sayid <sayid.salizhanov@mail.ru>
2024-12-19 17:38:24 +0300

    удалены излишние комментарии кода

--- src/main/webapp/js/group/adminApplications.js
+++ src/main/webapp/js/group/adminApplications.js
@@ -3,27 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     actionForms.forEach(form => {
         form.addEventListener("submit", function(event) {
-            event.preventDefault(); // Остановить стандартное поведение формы
+            event.preventDefault();
 
             const formData = new FormData(this);
             const applicationId = formData.get("applicationId");
             const approveButton = document.querySelector(`#approve-button-${applicationId}`);
             const rejectButton = document.querySelector(`#reject-button-${applicationId}`);
 
-            // Отправляем запрос вручную
             fetch(this.action, {
                 method: this.method,
-                body: new URLSearchParams(formData) // Преобразуем FormData в URLSearchParams
+                body: new URLSearchParams(formData)
             }).then(response => {
                 if (response.ok) {
                     console.log("Запрос успешен");
-                    // Делаем обе кнопки неактивными
                     approveButton.classList.add("disabled");
                     approveButton.setAttribute("disabled", "disabled");
                     rejectButton.classList.add("disabled");
                     rejectButton.setAttribute("disabled", "disabled");
 
-                    // Меняем текст кнопки в зависимости от того, какая была нажата
                     if (formData.get("applicationStatus") === "Одобрено") {
                         approveButton.textContent = "Принято";
                     } else {
