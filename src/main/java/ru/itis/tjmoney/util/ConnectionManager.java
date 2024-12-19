a88c4224ff27bd9cd1582b371dbbc606e497d32c
Sayid <sayid.salizhanov@mail.ru>
2024-12-19 23:06:00 +0300

    данные для подключения бд вынесены в отдельный файл

--- src/main/java/ru/itis/tjmoney/util/ConnectionManager.java
+++ src/main/java/ru/itis/tjmoney/util/ConnectionManager.java
@@ -1,13 +1,14 @@
 package ru.itis.tjmoney.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public final class ConnectionManager {
-    private static final String URL = "jdbc:postgresql://localhost:5432/tj-money";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String PROPERTIES_FILE = "database.properties";
 
     private static Connection connection;
 
@@ -17,9 +18,21 @@ public final class ConnectionManager {
     public static Connection getConnection() {
         if (connection == null) {
             try {
-                Class.forName("org.postgresql.Driver");
-                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            } catch (SQLException | ClassNotFoundException e) {
+                Properties properties = new Properties();
+                try (InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+                    if (input == null) {
+                        throw new RuntimeException("Sorry, unable to find " + PROPERTIES_FILE);
+                    }
+                    properties.load(input);
+                }
+                String url = properties.getProperty("db.url");
+                String username = properties.getProperty("db.username");
+                String password = properties.getProperty("db.password");
+                String driverPath = properties.getProperty("db.driver");
+
+                Class.forName(driverPath);
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException | ClassNotFoundException | IOException e) {
                 throw new RuntimeException(e);
             }
         }
